3d503c4ae4cf5d25b38ccc6f7553747fd37e8312
Alfred <somethingfly@somethingfly.com>
2022-07-23 16:42:06 -0700

    commit tree 5cb2d6c9215399138a54747ee868134b5babc0cb

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    const textParagraph = this.getElementById("text");
+    textParagraph.textContent = "This is really cool!";
+  });
+  
